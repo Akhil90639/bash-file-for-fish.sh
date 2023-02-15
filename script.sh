8166d5975142bad6f2ede887b2849c03df678f72
EC2 Default User <ec2-user@ip-172-31-19-195.ec2.internal>
2023-02-15 18:49:08 +0000

    this is python second file

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo apt update
 sudo apt-get full upgrade -y
-sudo apt install git
 sudo apt-get install python3-pip -y
-git clone https://github.com/Akhil90639/fish.git
-cd fish
+sudo apt-get install git -y
+git clone https://github.com/Akhil90639/indian-liver-patients.git
+cd indian-liver-patients
 sudo pip3 install -r requirements.txt
 python3 app.py
